806f81a7ba866a3e6e2d9497904ee62ce65dbd0e
autodistries <automatedactions@protonmail.com>
2024-08-21 22:15:59 +0200

    final testing update. Should be able to upd mods. Will branch into a brand-new mods updater

--- ModsUpdater/ModsUpdater.cs
+++ ModsUpdater/ModsUpdater.cs
@@ -10,6 +10,9 @@ using System.Threading.Tasks;
 using System.Text.RegularExpressions;
 using System.Net.Http;
 using UnityEngine;
+using Menu.Remix;
+using Menu;
+using static ModsUpdater.Utils;
 namespace ModsUpdater;
 
 [BepInPlugin(PLUGIN_GUID, PLUGIN_NAME, PLUGIN_VERSION)]
@@ -77,47 +80,53 @@ public partial class ModsUpdater : BaseUnityPlugin
         /*menuTab = new ConfigMenuTab();*/
         // modButtons.Add(new ModButton(configMenuTab.modList, 0));
         lls.LogDebug("3.5");
-        modOptions.AddButtonOption(40, "load local mods", loadLocalMods);
-        modOptions.AddButtonOption(80, "list local mods", listLocalMods);
-        modOptions.AddButtonOption(120, "download remote", downloadRemoteModsList);
-        modOptions.AddButtonOption(160, "load server mods", loadServerMods);
-        modOptions.AddButtonOption(200, "dl first mod", tryToUpdateFirstMod);
-        modOptions.AddButtonOption(240, "summon new tab", tryToCreateTab2);
-        modOptions.AddButtonOption(280, "modOptions.ResetUIelements", trytoaaa);
-        modOptions.AddButtonOption(320, "modOptions. manual reset !!", trytobbb);
-        modOptions.AddButtonOption(360, "summon OpScrollBox", trytoopscrollbox);
+        modOptions.AddButtonOption(40, "set update mods", setUpdMods);
+        modOptions.AddButtonOption(80, "set download mods", setDlMods);
+        modOptions.AddButtonOption(120, "set text toaa", setTxt);
+        modOptions.AddButtonOption(160, "make it dirtyy", setdirty);
         lls.LogDebug("3.6");
         /*if (ConfigContainer.menuTab.modList.GetModButton(ModManager.InstalledMods[i].id) == null)*/
         /*sortedModButtons.AddRange(list);*/
     }
 
-    private void trytoopscrollbox(UIfocusable trigger)
+    private void setdirty(UIfocusable trigger)
     {
-       OpScrollBox opScrollBox = new OpScrollBox(new Vector2(30f, 80f), new Vector2(250f, 450f), 25f, false, true, true);
-       modOptions.Tabs[0].AddItems(opScrollBox);
-       /* In the end we need:
-       a button to update all available
-       a button & text for update and info abt raindb.js*/
+        modOptions.localModsContainer.listIsDirty = true;
+    }
+
+    private void setTxt(UIfocusable trigger)
+    {
+        ModOptions.SetInfoLabel("zngjjjjjjjjjjjjjjjjjjjjjj");
+    }
+
+    private void setDlMods(UIfocusable trigger)
+    {
+        modOptions.localModsContainer.CurrentContainerStatus = ModOptions.ModsContainer.ContainerStatus.Download;
+    }
+
+    private void setUpdMods(UIfocusable trigger)
+    {
+        modOptions.localModsContainer.CurrentContainerStatus = ModOptions.ModsContainer.ContainerStatus.Update;
     }
 
     private void trytobbb(UIfocusable trigger)
     {
         OpTab[] tabs = modOptions.Tabs;
-		for (int i = 0; i < tabs.Length; i++)
-		{
-			foreach (UIelement item in tabs[i].items)
-			{
-				if (item is UIconfig && (item as UIconfig).cosmetic)
-				{
-					item.Reset();
-				}
-			}
-		}
+        for (int i = 0; i < tabs.Length; i++)
+        {
+            foreach (UIelement item in tabs[i].items)
+            {
+                if (item is UIconfig && (item as UIconfig).cosmetic)
+                {
+                    item.Reset();
+                }
+            }
+        }
     }
 
     private void trytoaaa(UIfocusable trigger)
     {
-                modOptions.ResetUIelements();
+        modOptions.ResetUIelements();
 
     }
 
@@ -152,29 +161,7 @@ public partial class ModsUpdater : BaseUnityPlugin
         button.text = "Please wait...";
         button.description = button.text;
 
-        string url = "https://raw.githubusercontent.com/AndrewFM/RainDB/master/raindb.js";
-        string targetPath = Path.Combine(THISMODPATH, "raindb.js");
 
-        int result = await FileManager.DownloadFileIfNewerAsync(url, targetPath);
-        switch (result)
-        {
-            case 0:
-                button.text = "Updated source";
-                button.description = "Local file was updated";
-                break;
-            case 1:
-                button.text = "Nothing to do";
-                button.description = "Local file is already up-to-date";
-                break;
-            case -1:
-                button.text = "Error";
-                button.description = "Could not get etag from headers";
-                break;
-            case -2:
-                button.text = "Error";
-                button.description = "You might not be connected to the internet";
-                break;
-        }
     }
 
     private void listLocalMods(UIfocusable trigger)
@@ -184,7 +171,7 @@ public partial class ModsUpdater : BaseUnityPlugin
             string temp = "-> ";
             try
             {
-                foreach (int i in Utils.VersionToList(mod.Version))
+                foreach (int i in Utils.VersionManager.VersionToList(mod.Version))
                 {
                     temp += i.ToString() + "-";
                 }
@@ -194,10 +181,10 @@ public partial class ModsUpdater : BaseUnityPlugin
         }
     }
 
-/// <summary>
-/// WE NEED TO IGNORE STEAMWORKS MODS!!
-/// </summary>
-/// <param name="focusable"></param>
+    /// <summary>
+    /// WE NEED TO IGNORE STEAMWORKS MODS!!
+    /// </summary>
+    /// <param name="focusable"></param>
     public void loadLocalMods(UIfocusable focusable)
     {
 
@@ -213,64 +200,7 @@ public partial class ModsUpdater : BaseUnityPlugin
     /// as such, needs localMods to be filled first !
     /// </summary>
     /// <param name="uIfocusable"></param>
-    public void loadServerMods(UIfocusable uIfocusable)
-    {
-        if (localMods.Count == 0)
-        {
-            lls.LogWarning("localMods wasn't loaded before !");
-            return;
-        }
-        string[] lines = File.ReadAllLines(Path.Combine(THISMODPATH, "raindb.js"));
-        string currentWorkingID = "";
-        string currentWorkingVersion = "";
-        string currentWorkingLink = "";
-
-        string detectionPattern = @"""(?<key>id|version|url)""\s*:\s*""(?<value>[^""]*)""\s*,?";
-
-        bool skippingThisMod = false;
-        foreach (string line in lines.Skip(2))
-        {
-            if (line == "")
-            {
-                if (skippingThisMod) skippingThisMod = false;
-                continue;
-            }
-            if (skippingThisMod) continue;
-            else if (line == "Mods.push({")
-            {
-                currentWorkingID = "undefined";
-                currentWorkingLink = "undefined";
-                currentWorkingVersion = "undefined";
-            }
-            else if (line == "") continue;
-            else if (line == "});")
-            {
-                lls.LogDebug($"adding {currentWorkingID} {currentWorkingVersion} {currentWorkingLink}");
-                serverMods.Add(new ServerMod(currentWorkingID, currentWorkingVersion, currentWorkingLink));
-            }
-            else
-            {
-                var match = Regex.Match(line, detectionPattern);
-                if (match.Success)
-                {
-                    string key = match.Groups["key"].Value;
-                    string value = match.Groups["value"].Value;
-                    //  lls.LogDebug($"matched {key} {value}");
-                    switch (key)
-                    {
-                        case "id":
-                            {
-                                if (!localMods.Any((lmod) => lmod.ID == value)) { skippingThisMod = true; break; }
-                                currentWorkingID = value; break;
-                            }
-                        case "version": currentWorkingVersion = value; break;
-                        case "url": currentWorkingLink = value; break;
-                    }
-                }
-            }
-        }
-
-    }
+   
 
 
 
@@ -311,7 +241,8 @@ public class LocalMod
     string version;
     string path;
 
-    public string Path {
+    public string Path
+    {
         get => path;
     }
     public string Version
@@ -341,7 +272,8 @@ public class ServerMod : LocalMod
 {
     string link;
 
-    public string Link {
+    public string Link
+    {
         get => link;
     }
     public ServerMod(string id, string version, string link) : base(id, version, "")
@@ -351,110 +283,3 @@ public class ServerMod : LocalMod
 }
 
 
-public class FileManager
-{
-    public static async Task<int> DownloadFileIfNewerAsync(string url, string localPath)
-    {
-        using (HttpClient client = new HttpClient())
-        {
-            try
-            {
-                // Send a HEAD request to get the ETag header
-                var headResponse = await client.SendAsync(new HttpRequestMessage(HttpMethod.Head, url));
-                headResponse.EnsureSuccessStatusCode();
-
-                // Get the ETag header
-                if (headResponse.Headers.ETag != null)
-                {
-                    string remoteETag = headResponse.Headers.ETag.Tag.Trim('"');
-
-                    // Check if the local file exists
-                    if (File.Exists(localPath))
-                    {
-                        // Read the local ETag from a file or a simple text file
-                        string localETagPath = localPath + ".etag";
-                        string localETag = File.Exists(localETagPath) ? File.ReadAllText(localETagPath) : null;
-
-                        // Compare the ETags
-                        if (remoteETag != localETag)
-                        {
-                            Console.WriteLine("Remote file is newer. Downloading...");
-                            await DownloadFileAsync(url, localPath);
-                            // Update the local ETag
-                            File.WriteAllText(localETagPath, remoteETag);
-                            return 0; // File was downloaded
-                        }
-                        else
-                        {
-                            Console.WriteLine("Local file is up to date.");
-                            return 1; // Local file is up to date
-                        }
-                    }
-                    else
-                    {
-                        // If the local file does not exist, download it
-                        Console.WriteLine("Local file does not exist. Downloading...");
-                        await DownloadFileAsync(url, localPath);
-                        // Save the ETag
-                        File.WriteAllText(localPath + ".etag", remoteETag);
-                        return 0; // File was downloaded
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("Could not retrieve ETag header.");
-                    return -1; // Error occurred
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error checking remote file: {ex.Message}");
-                return -2; // Error occurred
-            }
-        }
-    }
-
-    private static async Task DownloadFileAsync(string url, string localPath)
-    {
-        using (HttpClient client = new HttpClient())
-        {
-            try
-            {
-                // Send a GET request to the specified URL
-                HttpResponseMessage response = await client.GetAsync(url);
-                response.EnsureSuccessStatusCode(); // Throw if not a success code.
-
-                // Read the response content as a byte array
-                byte[] fileBytes = await response.Content.ReadAsByteArrayAsync();
-
-                // Write the byte array to a file asynchronously using FileStream
-                using (FileStream fs = new FileStream(localPath, FileMode.Create, FileAccess.Write, FileShare.None))
-                {
-                    await fs.WriteAsync(fileBytes, 0, fileBytes.Length);
-                }
-
-                Console.WriteLine($"File downloaded successfully to {localPath}");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine($"Error downloading file: {ex.Message}");
-            }
-        }
-    }
-
-    public static async Task<int> GetUpdateAndUnzip(string url, string modPath) {
-        if (!url.EndsWith("zip")) return -1;
-        if (!(Directory.GetParent(Path.GetFullPath(modPath)).ToString() == ModsUpdater.MODSPATH)) {
-            return -2;
-        }
-        string fileName = url.Split('/').Last();
-        string tempFilePath = Path.Combine(ModsUpdater.THISMODPATH, "." + fileName);
-        await DownloadFileAsync(url, tempFilePath);
-        Console.WriteLine(ModsUpdater.MODSPATH);
-        System.IO.Directory.Delete(modPath, true);
-        System.IO.Directory.CreateDirectory(modPath);
-        System.IO.Compression.ZipFile.ExtractToDirectory(tempFilePath, modPath);
-        return 0;
-    }
-
-}
--- ModsUpdater/Utils.cs
+++ ModsUpdater/Utils.cs
@@ -2,70 +2,216 @@
 
 using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
 using IL.Menu;
 using IL.MoreSlugcats;
 
-public static class Utils {
-    // Only supports . as a separator !
-    public static bool IsVersionGreater(string a, string b) {
-        if (!CheckVersionValidity(a) || !CheckVersionValidity(b)){
-            
-            return false;}
-        
-        List<Int32> versionA = VersionToList(a);
-        List<Int32> versionB = VersionToList(b);
+namespace ModsUpdater;
 
-        for (int i = 0; i<Math.Max(versionA.Count, versionB.Count); i++) {
-            if (versionA.Count > i && versionB.Count > i) {
-                if (versionB[i]>versionA[i]) return true;
-                else if (versionA[i] > versionB[i]) return false;
-            } else if (versionB.Count>versionA.Count && versionB[i] != 0) return true;
+public static class Utils
+{
+
+
+    public static class VersionManager
+    {
+        // Only supports . as a separator !
+        public static bool IsVersionGreater(string a, string b)
+        {
+            if (a == null || b==null ||!CheckVersionValidity(a) || !CheckVersionValidity(b))
+            {
+
+                return false;
+            }
+
+            List<Int32> versionA = VersionToList(a);
+            List<Int32> versionB = VersionToList(b);
+
+            for (int i = 0; i < Math.Max(versionA.Count, versionB.Count); i++)
+            {
+                if (versionA.Count > i && versionB.Count > i)
+                {
+                    if (versionB[i] > versionA[i]) return true;
+                    else if (versionA[i] > versionB[i]) return false;
+                }
+                else if (versionB.Count > versionA.Count && versionB[i] != 0) return true;
+            }
+            return false;
         }
-        return false;
+
+        public static List<int> VersionToList(string a)
+        {
+            List<int> res = new();
+            string[] splittedA = a.Split('.');
+            foreach (string s in splittedA)
+            {
+                if (Int32.TryParse(s, out int localVersion))
+                {
+                    res.Add(localVersion);
+                }
+                else
+                {
+                    //separate string from rest
+                    // in no case should the version letter(s) be before an int!!
+                    string fp = "";
+                    string dp = "";
+                    for (int i = 0; i < s.Length; i++)
+                    {
+                        char c = s[i];
+                        if (c >= 48 /*0*/ && c <= 57 /*9*/)
+                        {
+                            fp += c.ToString();
+                        }
+                        else if (c >= 97 /*a*/ && c <= 122 /*z*/)
+                        {
+                            if (i != s.Length - 1) throw new FormatException("version string was not in correct format: found number after letter");
+                            dp = (c - 96).ToString();
+                        }
+                        else throw new FormatException("version string was not in correct format: found foreign character");
+                    }
+                    if (fp.Length != 0) res.Add(Int32.Parse(fp));
+                    if (dp.Length != 0) res.Add(Int32.Parse(dp));
+                }
+            }
+            return res;
+        }
+
+        /// <summary>
+        /// checks version validity. Numbers only; one letter allowed at the end. Separator is .
+        /// </summary>
+        /// <param name="s"></param>
+        /// <returns></returns>
+        public static bool CheckVersionValidity(string s)
+        {
+            if (s.Length==0) return false;
+            for (int i = 0; i < s.Length; i++)
+            {
+                char c = s[i];
+                if (c >= 48 /*0*/ && c <= 57 /*9*/ || c >= 97 /*a*/ && c <= 122 /*z*/ || c == '.')
+                {
+                    if (c >= 97 /*a*/ && c <= 122 /*z*/)
+                    {
+                        if (i != s.Length - 1) return false;
+                    }
+                }
+                else return false;
+            }
+            return true;
+        }
+
     }
 
-    public static List<int> VersionToList(string a)
+
+    public class FileManager
     {
-        List<int> res = new();
-        string[] splittedA = a.Split('.');
-        foreach(string s in splittedA) {
-            if (Int32.TryParse(s, out int localVersion)) {
-                res.Add(localVersion);
-            } else {
-                //separate string from rest
-                // in no case should the version letter(s) be before an int!!
-                string fp = "";
-                string dp = "";
-                for (int i=0; i<s.Length;i++) {
-                    char c= s[i];
-                    if (c >= 48 /*0*/ && c <= 57 /*9*/) {
-                        fp += c.ToString();
-                    } else if (c>=97 /*a*/ && c<=122 /*z*/) {
-                        if (i!=s.Length-1) throw new FormatException("version string was not in correct format: found number after letter");                        
-                        dp =(c-96).ToString();
-                    } else throw new FormatException("version string was not in correct format: found foreign character");
+        public static async Task<int> DownloadFileIfNewerAsync(string url, string localPath)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    // Send a HEAD request to get the ETag header
+                    var headResponse = await client.SendAsync(new HttpRequestMessage(HttpMethod.Head, url));
+                    headResponse.EnsureSuccessStatusCode();
+
+                    // Get the ETag header
+                    if (headResponse.Headers.ETag != null)
+                    {
+                        string remoteETag = headResponse.Headers.ETag.Tag.Trim('"');
+
+                        // Check if the local file exists
+                        if (File.Exists(localPath))
+                        {
+                            // Read the local ETag from a file or a simple text file
+                            string localETagPath = localPath + ".etag";
+                            string localETag = File.Exists(localETagPath) ? File.ReadAllText(localETagPath) : null;
+
+                            // Compare the ETags
+                            if (remoteETag != localETag)
+                            {
+                                Console.WriteLine("Remote file is newer. Downloading...");
+                                await DownloadFileAsync(url, localPath);
+                                // Update the local ETag
+                                File.WriteAllText(localETagPath, remoteETag);
+                                return 0; // File was downloaded
+                            }
+                            else
+                            {
+                                Console.WriteLine("Local file is up to date.");
+                                return 1; // Local file is up to date
+                            }
+                        }
+                        else
+                        {
+                            // If the local file does not exist, download it
+                            Console.WriteLine("Local file does not exist. Downloading...");
+                            await DownloadFileAsync(url, localPath);
+                            // Save the ETag
+                            File.WriteAllText(localPath + ".etag", remoteETag);
+                            return 0; // File was downloaded
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Could not retrieve ETag header.");
+                        return -1; // Error occurred
+                    }
                 }
-                if (fp.Length != 0) res.Add(Int32.Parse(fp));
-                if (dp.Length!=0) res.Add(Int32.Parse(dp));
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Error checking remote file: {ex.Message}");
+                    return -2; // Error occurred
+                }
             }
         }
-        return res;
-    }
 
-    /// <summary>
-    /// checks version validity. Numbers only; one letter allowed at the end. Separator is .
-    /// </summary>
-    /// <param name="s"></param>
-    /// <returns></returns>
-    public static bool CheckVersionValidity(string s) {
-        for (int i=0; i<s.Length;i++) {
-            char c = s[i];
-            if (c >= 48 /*0*/ && c <= 57 /*9*/ || c>=97 /*a*/ && c<=122 /*z*/ || c=='.') {
-                if (c>=97 /*a*/ && c<=122 /*z*/) {
-                    if (i != s.Length-1) return false;
+        public static async Task<int> DownloadFileAsync(string url, string localPath)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                try
+                {
+                    // Send a GET request to the specified URL
+                    HttpResponseMessage response = await client.GetAsync(url);
+                    response.EnsureSuccessStatusCode(); // Throw if not a success code.
+
+                    // Read the response content as a byte array
+                    byte[] fileBytes = await response.Content.ReadAsByteArrayAsync();
+
+                    // Write the byte array to a file asynchronously using FileStream
+                    using (FileStream fs = new FileStream(localPath, FileMode.Create, FileAccess.Write, FileShare.None))
+                    {
+                        await fs.WriteAsync(fileBytes, 0, fileBytes.Length);
+                    }
+
+                    return 0;
+
                 }
-            } else return false;
+                catch (Exception ex)
+                {
+                    return -1;
+                }
+            }
         }
-        return true;
+
+        public static async Task<int> GetUpdateAndUnzip(string url, string modPath)
+        {
+            if (!url.EndsWith("zip")) return -1;
+            if (!(Directory.GetParent(Path.GetFullPath(modPath)).ToString() == ModsUpdater.MODSPATH))
+            {
+                return -2;
+            }
+            string fileName = url.Split('/').Last();
+            string tempFilePath = Path.Combine(ModsUpdater.THISMODPATH, "." + fileName);
+            await DownloadFileAsync(url, tempFilePath);
+            Console.WriteLine(ModsUpdater.MODSPATH);
+            System.IO.Directory.Delete(modPath, true);
+            System.IO.Directory.CreateDirectory(modPath);
+            System.IO.Compression.ZipFile.ExtractToDirectory(tempFilePath, modPath);
+            return 0;
+        }
+
     }
 }
