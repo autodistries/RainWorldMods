54c9699f58c597d062316e0a4d3ec4629c9d95c6
Autodistries <autodistries@protonmail.com>
2024-12-09 20:36:30 +0100

    add mod template

---
+++ ModTemplate/ModName.cs
@@ -0,0 +1,41 @@
+﻿using BepInEx;
+
+// Access private fields and stuff
+using System.Security;
+using System.Security.Permissions;
+[module: UnverifiableCode]
+[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+
+
+namespace ModName;
+
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+
+
+public partial class ModMainClass : BaseUnityPlugin
+{
+    BepInEx.Logging.ManualLogSource lls;
+
+    bool done = false;
+
+
+    public ModMainClass()
+    {
+        lls = BepInEx.Logging.Logger.CreateLogSource("Mod Name");                     
+    }
+
+    private void Awake()
+    {
+        if (done) return;
+        lls.LogInfo("Hello World !");
+        On.RainWorld.OnModsInit += RainWorldOnOnModsInitDetour;
+        done = true;
+    }
+
+  
+
+    private void RainWorldOnOnModsInitDetour(On.RainWorld.orig_OnModsInit orig, RainWorld self)
+    {
+        orig(self);
+    }   
+}
---
+++ ModTemplate/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace ModName
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "nope.modname";
+        public const string PLUGIN_NAME = "Mod name";
+        public const string PLUGIN_VERSION = "0.0.2";
+    }
+}
