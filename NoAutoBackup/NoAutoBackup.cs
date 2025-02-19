4ea8b85eb8a3102ebb7b06300acbdc0fec25a60b
catsoft <not.a@cat.here>
2025-02-19 20:20:35 +0000

    initial and final release probably

---
+++ NoAutoBackup/NoAutoBackup.cs
@@ -0,0 +1,41 @@
+﻿using BepInEx;
+
+
+
+namespace NoAutoBackup; //this needs to be changed accordingly, if you rename ModName.csproj ! Explanations on top of ModName.csproj
+
+[BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+
+
+public partial class BackupsDisabler : BaseUnityPlugin
+{
+    BepInEx.Logging.ManualLogSource lls;
+
+    bool done = false;
+
+
+    public BackupsDisabler()
+    {
+        lls = BepInEx.Logging.Logger.CreateLogSource(GetType().ToString());                     
+    }
+
+    private void Awake()
+    {
+        if (done) return;
+        lls.LogInfo("Hello World !");
+        On.RainWorld.OnModsInit += RainWorldOnOnModsInitDetour;
+        On.PlayerProgression.CreateCopyOfSaves_bool += PreventSavingIfNotUserInitiated;
+        done = true;
+    }
+
+    private void PreventSavingIfNotUserInitiated(On.PlayerProgression.orig_CreateCopyOfSaves_bool orig, PlayerProgression self, bool userCreated)
+    {
+        if (!userCreated) return;
+        orig(self, userCreated);
+    }
+
+    private void RainWorldOnOnModsInitDetour(On.RainWorld.orig_OnModsInit orig, RainWorld self)
+    {
+        orig(self);
+    }   
+}
---
+++ NoAutoBackup/PluginInfo.cs
@@ -0,0 +1,10 @@
+// This file was generated on build by the .csproj
+namespace NoAutoBackup
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "nope.no.auto.backup";
+        public const string PLUGIN_NAME = "Auto-backup disabler";
+        public const string PLUGIN_VERSION = "1.0";
+    }
+}
