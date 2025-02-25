5fff13cd3573c51cb0cf28cba173b314d705a01e
catsoft <not.a@cat.here>
2025-02-25 13:56:20 +0000

    Bump cs version, add some info

--- ModTemplate/ModName.cs
+++ ModTemplate/ModName.cs
@@ -1,34 +1,32 @@
 ﻿using BepInEx;
 
+
 // Access private fields and stuff
-using System.Security;
 using System.Security.Permissions;
-#pragma warning disable CS0618 // SecurityAction.RequestMinimum is obsolete. However, this does not apply to the mod, which still needs it. Suppress the warning indicating that it is obsolete.
-[module: UnverifiableCode]
+#pragma warning disable CS0618
 [assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
 #pragma warning restore CS0618
 
 namespace ModName; //this needs to be changed accordingly, if you rename ModName.csproj ! Explanations on top of ModName.csproj
 
+// These properties are properly set when building, so errors are to be expected in your IDE, but building will occur properly.
+// If not, please check the .csproj !
 [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
 
 
 public partial class ModMainClass : BaseUnityPlugin
 {
-    BepInEx.Logging.ManualLogSource lls;
 
     bool done = false;
 
-
     public ModMainClass()
     {
-        lls = BepInEx.Logging.Logger.CreateLogSource("Mod Name");                     
     }
 
     private void Awake()
     {
         if (done) return;
-        lls.LogInfo("Hello World !");
+        Logger.LogInfo("Hello World !");
         On.RainWorld.OnModsInit += RainWorldOnOnModsInitDetour;
         done = true;
     }
